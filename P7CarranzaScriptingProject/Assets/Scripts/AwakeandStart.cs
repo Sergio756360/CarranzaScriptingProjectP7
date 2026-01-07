b1d5ee9849c2965db846a12e03ad702e2bd1c100
Sergio756360 <756360@student.dusd.net>
2026-01-07 14:15:29 -0800

    Awake and Start
    Newest addition of the Scripting project, activates both the Awake and Start functions at the same time

---
+++ P7CarranzaScriptingProject/Assets/Scripts/AwakeandStart.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Awakening : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Awake is called.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Start is called.");
+    }
+}
