1c26ff71e365d2e0f599a18cc70a9b77ad1fe33a
Sergio756360 <756360@student.dusd.net>
2026-02-09 14:31:00 -0800

    Destroy
    Added scripts for the Destroy command

---
+++ P7CarranzaScriptingProject/Assets/Scripts/DestroyOther.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject other; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            Destroy(other);
+        }
+    }
+}
