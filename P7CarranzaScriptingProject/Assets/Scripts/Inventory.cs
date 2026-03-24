0386c838af813f78718244f752cc5795ae8d71c4
Sergio756360 <756360@student.dusd.net>
2026-03-24 14:47:11 -0700

    Classes
    Tank

---
+++ P7CarranzaScriptingProject/Assets/Scripts/Inventory.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int bullets;
+        public int grenades;
+        public int rockets;
+        public float fuel;
+
+        public Stuff(int bul, int gre, int roc)
+        {
+            bullets = bul;
+            grenades = gre;
+            rockets = roc;
+        }
+
+        public Stuff(int bul, float fu)
+        {
+            bullets = bul;
+            fuel = fu;
+        }
+
+        //Constructor 
+        public Stuff()
+        {
+            bullets = 1;
+            grenades = 1;
+            rockets = 1;
+        }
+
+
+
+    }
+    
+    // Creating an Instance (an Object) of the Stuff class
+    public Stuff myStuff = new Stuff(50, 5, 5);
+
+    public Stuff myOtherStuff = new Stuff(50, 1.5f);
+
+
+    // Update is called once per frame
+    void Start()
+    {
+        Debug.Log(myStuff.bullets);
+    }
+}
