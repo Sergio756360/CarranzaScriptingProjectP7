c5b8d81ea0eb1399ca1da2dba14012da64f66794
Sergio756360 <756360@student.dusd.net>
2026-09-04 13:05:18 -0700

    Creating Properties
    First scripting project of the year

---
+++ P5CarranzaScriptingProject/Assets/Scripts/Properties/GameProperties.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameProperties : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerProperties myPlayer = new PlayerProperties();
+
+        //Properties can be used just like variables
+        myPlayer.Experience = 5;
+        int x = myPlayer.Experience;
+    }
+}
